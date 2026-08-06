3e12999b5d44be24ce94d0146a8e8c333c34f698
Rajveer Rajput <rajveerhada09@gmail.com>
2026-08-06 18:08:00 +0530

    feat(auth): complete user module foundation

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,22 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-import 'features/auth/presentation/screens/login_screen.dart';
 import 'firebase_options.dart';
+import 'features/user/providers/user_provider.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(
-    const ProviderScope(
-      child: VexaApp(),
-    ),
-  );
+  runApp(const VexaApp());
 }
 
 class VexaApp extends StatelessWidget {
@@ -24,14 +21,21 @@ class VexaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Vexa',
-      theme: ThemeData(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserProvider>(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Vexa',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
         brightness: Brightness.dark,
         useMaterial3: true,
+),
+        home: const LoginScreen(),
       ),
-      home: const LoginScreen(),
     );
   }
 }
